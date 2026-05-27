954ec97d3665c2968177dca580f1961fd34174bf
Ahad khan <157043546+Ahad0p@users.noreply.github.com>
2026-05-27 17:15:22 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -7,7 +7,7 @@ def call(String Project, String ImageTag, String dockerhubuser){
             passwordVariable: 'DOCKER_PASS'
         )
     ]) {
-
+        echo "docker push loaded"
         sh """
             docker login -u \$DOCKER_USER -p \$DOCKER_PASS
             docker push ${dockerhubuser}/${Project}:${ImageTag}
